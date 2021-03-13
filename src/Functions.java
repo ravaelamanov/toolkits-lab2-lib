a4d5fb363db657df70cd50dd4f6078c6b5ff1599
Ravshan Elamanov <ravaelamanov@gmail.com>
2021-03-13 16:28:24 +0300

    Initial commit

---
+++ src/Functions.java
@@ -0,0 +1,13 @@
+public class Functions {
+    public static double sqrt(double val) {
+        return Math.sqrt(val);
+    }
+
+    public static double pow(double val, double pow) {
+        return Math.pow(val, pow);
+    }
+
+    public static double sin(double x) {
+        return Math.sin(x);
+    }
+}
